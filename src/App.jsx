d86f064e939611d992b0e751199b66ec581ad385
Pablo Lires <pablolires@gmail.com>
2025-01-28 12:37:39 -0300

    Cambio de orden del componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '@/pages/Home'
 import NotFound from '@/pages/NotFound'
-import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
 function App() {
   return (
     <>
-      <Header />
       <Router basename='/landing/'>
         <main>
           <Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import ContentProductos from '@/components/ContentProductos.jsx'
 import Features from '@/components/Features.jsx'
 import CarruselImagesGallery from '@/components/CarruselImagesGallery.jsx'
 import LastCTA from '@/components/LastCTA.jsx'
+import Header from '@/components/Header'
 import Whatsapp from '@/components/Whatsapp'
 
 const Home = () => {
   return (
     <section className='home'>
       <Whatsapp />
+      <Header />
       <ContentProductos />
       <Features />
       <CarruselImagesGallery />
